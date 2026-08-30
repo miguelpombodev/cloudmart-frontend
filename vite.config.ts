c8c5370a2dd09abb04404be8c728160671b160ec
Miguel Pombo <mp96dev@gmail.com>
2026-08-30 15:50:20 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import path from "path";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint2";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [eslint(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
