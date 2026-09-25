8edfc90d750e18cd513e8d1788a8b775c624828b
Miguel Pombo <mp96dev@gmail.com>
2026-09-25 16:47:47 +0100

    refactor: restyle hero

--- src/features/home/components/Hero/index.tsx
+++ src/features/home/components/Hero/index.tsx
@@ -2,35 +2,43 @@ import { Button } from "@/shared/components/Button";
 
 export function Hero() {
   const heroImgUrl =
-    "https://img.magnific.com/free-photo/modern-man-casual-outfit-showing-shopping-bag-okay-sign-winking-camera-recommending-shop_1258-300002.jpg";
+    "https://img.magnific.com/fotos-gratis/poltrona-amarela-na-sala-de-estar-com-espaco-para-texto_43614-940.jpg";
+
+  const heroImgSrcSet =
+    "https://img.magnific.com/fotos-gratis/poltrona-amarela-na-sala-de-estar-com-espaco-para-texto_43614-940.jpg?t=st=1790350222~exp=1790353822~hmac=7a43f1315d96721e3f9b6234b793cb9d3ddeb91af7386f6e16daf06b9eb63eef&w=360 360w, https://img.magnific.com/fotos-gratis/poltrona-amarela-na-sala-de-estar-com-espaco-para-texto_43614-940.jpg?t=st=1790350222~exp=1790353822~hmac=7a43f1315d96721e3f9b6234b793cb9d3ddeb91af7386f6e16daf06b9eb63eef&w=740 740w, https://img.magnific.com/fotos-gratis/poltrona-amarela-na-sala-de-estar-com-espaco-para-texto_43614-940.jpg?t=st=1790350222~exp=1790353822~hmac=7a43f1315d96721e3f9b6234b793cb9d3ddeb91af7386f6e16daf06b9eb63eef&w=1060 1060w, https://img.magnific.com/fotos-gratis/poltrona-amarela-na-sala-de-estar-com-espaco-para-texto_43614-940.jpg?t=st=1790350222~exp=1790353822~hmac=7a43f1315d96721e3f9b6234b793cb9d3ddeb91af7386f6e16daf06b9eb63eef&w=1480 1480w, https://img.magnific.com/fotos-gratis/poltrona-amarela-na-sala-de-estar-com-espaco-para-texto_43614-940.jpg?t=st=1790350222~exp=1790353822~hmac=7a43f1315d96721e3f9b6234b793cb9d3ddeb91af7386f6e16daf06b9eb63eef&w=2000 2000w";
 
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2">
-      <div className="flex flex-col items-center justify-center gap-7 p-8 order-2 md:p-9 md:order-1 md:items-start">
-        <h4 className="font-functional text-xs uppercase tracking-wide text-low-gray">
+    <section className="flex items-center relative min-h-[500px]">
+      <img
+        src={heroImgUrl}
+        srcSet={heroImgSrcSet}
+        alt="hero"
+        sizes="100vw"
+        className="absolute inset-0 w-full h-full object-cover object-center z-0"
+      />
+
+      <div className="absolute inset-0 bg-black/30 -z-10" />
+
+      <div className="relative z-10 flex flex-col items-center justify-center gap-7 p-8 order-2 md:p-9 md:order-1 md:items-start">
+        <h4 className="font-functional text-xs uppercase tracking-wide text-white/70">
           Current Collection
         </h4>
 
-        <h1 className="max-w-[450px] font-functional text-4xl leading-snug text-green-default md:text-[32px]">
+        <h1 className="max-w-[450px] font-functional text-4xl leading-snug text-white md:text-[32px]">
           A curated selection of varied products, chosen for their materials and
           finishes.
         </h1>
 
-        <p className="max-w-[280px] text-sm  text-low-gray">
+        <p className="max-w-[280px] text-sm text-white">
           A curated selection of varied products, chosen for their materials and
           finishes.
         </p>
 
-        <span className="flex items-center flex-1">
-          <Button kind="default" value="See collection" className="mt-1 " />
-        </span>
-      </div>
-
-      <div className="min-h-[280px] overflow-hidden order-1 md:min-h-[280px] md:order-2">
-        <img
-          src={heroImgUrl}
-          alt="Model showing a piece of the collection"
-          className="h-full min-h-[280px] w-full object-cover"
+        <Button
+          kind="default"
+          value="See collection"
+          className="mt-1"
+          sizeType="small"
         />
       </div>
     </section>
