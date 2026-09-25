b289ce73e3045ed1335e734f96d4b35389a612e8
Miguel Pombo <mp96dev@gmail.com>
2026-09-25 17:40:35 +0100

    fix: add margin-top at footer

--- src/shared/components/Footer/index.tsx
+++ src/shared/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="flex p-10 bg-white justify-center text-low-gray font-edit font-light border-t md:justify-between">
+    <footer className="flex mt-10 p-10 bg-white justify-center text-low-gray font-edit font-light border-t md:justify-between">
       <h3>©CloudMart</h3>
       <span className="hidden md:flex md:gap-5">
         <p>Deliveries</p>
